be0b702a926335f0c9ce360bad03cd45ea515976
jldiamond <root@LAPTOP-HBTDN1F7>
2024-01-22 11:08:25 -0500

    final commit!

--- server/routers/newsRouter.js
+++ server/routers/newsRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const newsController = require('../controllers/orderTicketController');
+const newsController = require('../controllers/newsController');
 const router = express.Router();
 
 router.get('/', newsController.news, (req, res) => {
   console.log('---> ENTERING NEWS ROUTER <---');
-  return res.status(200);
+  return res.status(200).send(res.locals.redditInfo);
 });
 
 module.exports = router;

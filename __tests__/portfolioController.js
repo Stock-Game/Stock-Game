6c5d7747e417808e69184b69cb0e049a26d0b7ae
rehcliw <rehcliw@gmail.com>
2024-01-23 13:56:51 -0500

    more portfolio controller tests

--- __tests__/portfolioController.js
+++ __tests__/portfolioController.js
@@ -1,18 +1,21 @@
-import subject from '../server/controllers/portfolioController.js';
+const subject = require('../server/controllers/portfolioController.js');
 const fs = require('fs');
 const path = require('path');
+const testJsonFile = path.resolve(__dirname, './portfolios.test.json');
 
 describe('portfolioController', () => {
   // dependencies: model.find, model.save, model.create, req, res, next
   // look at unit-12 db
   const mockModel = {
-    find: jest.fn(),
+    find: jest.fn(() => {
+      return JSON.parse(fs.readFileSync(testJsonFile));
+    }),
     save: jest.fn(),
     create: jest.fn(),
-  }
+  };
 
   describe('portfolioController.buy', () => {
-    const mockRequest = {
+    const mockReq = {
       body: {
         ticker: 'BABA',
         priceBought: 100,
@@ -21,10 +24,31 @@ describe('portfolioController', () => {
         totalCost: 400,
       },
     };
-    const mockResponse = {};
+    const mockRes = {};
     const mockNext = jest.fn();
-    
 
-    it('should ')
+    it('should call model.find', () => {
+      subject.buy(mockReq, mockRes, mockNext, mockModel);
+      expect(mockModel.find).toHaveBeenCalled();
+    });
+
+    describe('creating new portfolio', () => {
+      beforeEach(() => {
+        fs.writeFileSync(testJsonFile, JSON.stringify([]));
+      });
+      it('should not change the request', () => {
+        const reqPreBuy = mockReq;
+        subject.buy(mockReq, mockRes, mockNext, mockModel);
+        expect(mockReq).toEqual(reqPreBuy);
+      });
+      it('should call model.create when no ticket found', () => {
+        subject.buy(mockReq, mockRes, mockNext, mockModel);
+        expect(mockModel.create).toHaveBeenCalled();
+      });
+      xit('should create new portfolio into db when no ticket found', () => {
+        subject.buy(mockReq, mockRes, mockNext, mockModel);
+      });
+      xit('should add that new portfolio onto res.locals.buy', () => {});
+    });
   });
 });
